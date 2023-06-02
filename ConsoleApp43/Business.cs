fff251c7fba6284fd19212bab3bd940145b39783
Aakash8870630 <135289125+Aakash8870630@users.noreply.github.com>
2023-06-01 21:26:57 -0400

    Add files via upload

---
+++ ConsoleApp43/Business.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Business : PropertyDetails
+    {
+        private bool customerLobbies;
+
+        public bool CustomerLobbies { get => customerLobbies; set => customerLobbies = value; }
+        public override void AdditionalWork()
+        {
+            Console.WriteLine("Customer lobbies created");
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Business {0} and customer lobbies {1} hp", base.ToString(), customerLobbies ? "this" : "that");
+        }
+    }
+}
