f094ce6ce58d8e9949b34e536ea0942a8f3d297f
Aakash8870630 <135289125+Aakash8870630@users.noreply.github.com>
2023-06-01 21:16:57 -0400

    Initial commit
    initial commit done

---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment2
+{    
+    public enum HouseTypes
+    {
+        House = 1,
+        Business,
+        Farmers
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+            Console.ReadKey();
+        }
+
+        public void Run()
+        {
+            List<PropertyDetails> myList = new List<PropertyDetails>();
+            string answer = string.Empty;
+            do
+            {
+                string ageOfHouse = string.Empty;
+                decimal houseAge;
+                do
+                {
+                    Console.Write("Enter house age: ");
+                    ageOfHouse = Console.ReadLine();
+                    houseAge = decimal.Parse(ageOfHouse);
+                } while (ageOfHouse.Length == 0);
+
+                string sizeOfHouse = string.Empty;
+                decimal houseSize;
+                do
+                {
+                    Console.Write("Enter house size: ");
+                    sizeOfHouse = Console.ReadLine();
+                    houseSize = decimal.Parse(sizeOfHouse);
+                } while (sizeOfHouse.Length == 0);
+
+                string sizeOfPaddock = string.Empty;
+                decimal paddockSize;
+                do
+                {
+                    Console.Write("Enter paddock size: ");
+                    sizeOfPaddock = Console.ReadLine();
+                    paddockSize = decimal.Parse(sizeOfPaddock);
+                } while (sizeOfPaddock.Length == 0);
+
+                string ccNumber = string.Empty;
+                var v = new PropertyDetails();
+                do
+                {
+                    Console.Write("Enter credit card number: ");
+                    ccNumber = Console.ReadLine();
+                } while (!v.CheckCreditCard(ccNumber));
+
+                int ownerType = 0; //ownerType
+                string stringOwner = string.Empty;
+
+                do
+                {
+                    Console.Write("Enter property owner[1 - House, 2 - Business, 3 - Farmer]: ");
+                    stringOwner = Console.ReadLine();
+                } 
+                while (!(int.TryParse(stringOwner, out ownerType) && ownerType >= (int)HouseTypes.House && ownerType <= (int)HouseTypes.Farmers));
+
+                PropertyDetails theProperty = null;
+                switch (ownerType)
+                {
+                    case (int)HouseTypes.House:
+                        theProperty = new House() { HouseAge = houseAge, HouseSize = houseSize, PaddockSize = paddockSize, CCNumber = ccNumber };
+                        string sunRoomDesign = string.Empty;
+                        do
+                        {
+                            Console.Write("do you need a design of sun room?[Y/N]");
+                            sunRoomDesign = Console.ReadLine();
+                        } while (sunRoomDesign.Length != 1 || (sunRoomDesign != "Y" && sunRoomDesign != "N"));
+                        ((House)theProperty).SunRoom = (sunRoomDesign == "Y") ? true : false;
+                        break;
+
+                    case (int)HouseTypes.Business:
+                        theProperty = new Business() { HouseAge = houseAge, HouseSize = houseSize, PaddockSize = paddockSize, CCNumber = ccNumber };
+                        string customerLobbiesDesign = string.Empty;
+                        do
+                        {
+                            Console.Write("do you need a design of customer lobbies?[Y/N]");
+                            customerLobbiesDesign = Console.ReadLine();
+                        } while (customerLobbiesDesign.Length != 1 || (customerLobbiesDesign != "Y" && customerLobbiesDesign != "N"));
+                        ((Business)theProperty).CustomerLobbies = (customerLobbiesDesign == "Y") ? true : false;
+                        break;
+
+                    case (int)HouseTypes.Farmers:
+                        theProperty = new Farmers() { HouseAge = houseAge, HouseSize = houseSize, PaddockSize = paddockSize, CCNumber = ccNumber };
+                        string storageAreasDesign = string.Empty;
+                        do
+                        {
+                            Console.Write("do you need a design of grain storage areas?[Y/N]");
+                            storageAreasDesign = Console.ReadLine();
+                        } while (storageAreasDesign.Length != 1 || (storageAreasDesign != "Y" && storageAreasDesign != "N"));
+                        ((Farmers)theProperty).StorageAreas = (storageAreasDesign == "Y") ? true : false;
+                        break;
+                }
+
+                myList.Add(theProperty);
+
+                Console.Write("Do you want to enter more?[Y/N] ");
+                answer = Console.ReadLine();
+            } while (answer.Length == 0 || answer.Length > 1 || answer.ToUpper() == "Y" );
+
+            foreach(PropertyDetails v in myList)
+            {
+                Console.WriteLine(v);
+                v.CreateDesign();
+                v.EstimateWork();
+                v.ArrageWorkers();
+                v.AdditionalWork();
+            }
+        }
+    }
+}
---
+++ PropertyDetails.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class PropertyDetails
+    {
+        private decimal houseAge;
+        private decimal houseSize;
+        private decimal paddockSize;
+        private string ccNumber;
+
+        public decimal HouseAge { get => houseAge; set => houseAge = value; }
+
+        public decimal HouseSize { get => houseSize; set => houseSize = value; }
+
+        public decimal PaddockSize { get => paddockSize; set => paddockSize = value; }
+        public string CCNumber { get => ccNumber; set => ccNumber = value; }
+
+        private string PrintCreditCard()
+        {
+            char[] ccNumArr = CCNumber.ToCharArray();
+            for(int i = 4;i < 11;i++) 
+            { 
+                ccNumArr[i] ='X'; 
+            }
+            return new string(ccNumArr);
+        }
+
+        public bool CheckCreditCard(string chceckCCNumber)
+        {
+            bool result = false;
+            if (chceckCCNumber != null && chceckCCNumber != string.Empty)
+            {
+                char[] CCNumberArray = chceckCCNumber.ToCharArray();
+                if (CCNumberArray.Length == 16)
+                {
+                    for (int i = 0; i < CCNumberArray.Length; i++)
+                    {
+                        if (char.IsDigit(CCNumberArray[i]))
+                        {
+                            result = true;
+                        }
+                        else
+                        {
+                            result = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+
+        public void CreateDesign()
+        {
+            Console.WriteLine("Design created");
+        }
+
+        public void EstimateWork()
+        {
+            Console.WriteLine("Estimation done");
+        }
+
+        public void ArrageWorkers()
+        {
+            Console.WriteLine("Workers arranged");
+        }        
+
+        public virtual void AdditionalWork()
+        {
+            Console.WriteLine("Should not see this");
+        }
+
+        public override string ToString()
+        {
+            return string.Format("with age {0}, size {1}, paddock size {2}, credit card number {3}", houseAge, houseSize, paddockSize, PrintCreditCard());
+        }
+    }
+}
